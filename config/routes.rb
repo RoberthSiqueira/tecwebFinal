97ca3c23226cb6bd2099dfce1d4f1b4324da84f1
Roberth Siqueira <roberth.siqueira@gmail.com>
2017-06-01 21:14:43 -0300

    Create Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   	get 'pages/index'
  	root to: 'pages#index'
+ 	get '/auth/:provider/callback', to: 'sessions#create'
+ 	delete '/logout', to: 'sessions#destroy'
 end
