783cef4767f80d595bf568eba490175d9fba0ade
Roberth Siqueira <roberth.siqueira@gmail.com>
2017-06-10 15:41:26 -0300

    Add attributes email and age to User

---
+++ db/migrate/20170610183933_add_email_age_to_user.rb
@@ -0,0 +1,6 @@
+class AddEmailAgeToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :email, :string
+    add_column :users, :age, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602000541) do
+ActiveRecord::Schema.define(version: 20170610183933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170602000541) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.integer "age"
   end
 
 end
