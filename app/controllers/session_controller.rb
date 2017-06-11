14287fd9d6f3be990dff42d986d6245665c35cf0
Roberth Siqueira <roberth.siqueira@gmail.com>
2017-06-11 14:39:36 -0300

    Test error

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,12 +1,12 @@
 class SessionController < ApplicationController
 	def create
-		begin
+		# begin
 			@user = User.from_omniauth(request.env['omniauth.auth'])
 		    session[:user_id] = @user.id
 		    flash[:success] = "Bem vindo, #{@user.name}!"
-		rescue
-		    flash[:warning] = "Ocorreu um problema ao tnetar autenticar..."
-		end
+		# rescue
+		    flash[:warning] = "Ocorreu um problema ao tentar autenticar..."
+		# end
 		 redirect_to root_path
 	end
 	def destroy
